129759cc684f62f90e6fda06839f73e615b41b9a
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-11-22 19:41:12 -0700

    first save of Cinetrail app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
