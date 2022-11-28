474abb80798b14daf88043fdf5a253cca5e25645
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-11-28 16:36:01 -0700

    slider workds for getting image

--- src/components/SLider/Slider.js
+++ src/components/SLider/Slider.js
@@ -5,6 +5,9 @@ import "./Slider.css";
 function Slider({ apiKey, baseUrl }) {
   //create state for the upcoming movies
   const [upcomingMovies, setUpcomingMovies] = React.useState([]);
+  const [index, setIndex] = React.useState(0);
+  //baseurl for images
+  const imageBaseUrl = "https://image.tmdb.org/t/p/original";
 
   //call api for data when the component loads
   React.useEffect(() => {
@@ -18,7 +21,15 @@ function Slider({ apiKey, baseUrl }) {
       .catch((err) => console.log(err));
   }, []);
 
-  // return <div>{upcomingMovies[0].title}</div>;
+  const sliderStyle = {
+    backgroundImage: `url("${imageBaseUrl}${upcomingMovies[index]?.backdrop_path}")`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "60vh",
+  };
+
+  return <div style={sliderStyle}>{upcomingMovies[0]?.title}</div>;
 }
 
 export default Slider;
