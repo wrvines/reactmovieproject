74364b821a8f10d78e78036ae222bb3e7a6d4afd
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-12-27 09:33:51 -0700

    added mobile styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return <div className="footer">All Rights Reserved</div>;
 }
 
 export default Footer;
